29f49c0fe48406f848ff3a352e8eeeecc3bc30ad
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-04-07 17:42:57 +0530

    work

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,55 @@
 const express = require("express");
 const User = require("../models/users.models");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const middleware = require('../middleware');
 
 const router = express.Router();
+
+router.get("/:username", middleware.checkToken, async (req, res) => {
+    try {
+        const username = req.params.username;
+        const user = await User.findOne({ username: username });
+
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+
+        return res.json({
+            data: user,
+            username: username
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ msg: "Internal server error" });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const user = await User.findOne({ username: username });
+
+        if (!user) {
+            return res.status(403).json("Username incorrect");
+        }
+
+        if (user.password === password) {
+            let token = jwt.sign({ username: req.body.username }, config.key, {
+                expiresIn: "24h"
+            })
+            return res.json({ token: token, msg: "success" });
+        } else {
+            return res.status(403).json("Password is incorrect");
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ msg: "Internal server error" });
+    }
+});
+
 
 router.route("/register").post((req, res) => {
     console.log("inside the register");
@@ -21,19 +69,49 @@ router.route("/register").post((req, res) => {
         });
 });
 
-router.route("/update/:username").patch((req, res) => {
-    User.findOneAndUpdate(
-        { username: req.params.username },
-        { $set: { password: req.body.password } },
-        (err, result) => {
-            if (err) return res.status(500).json({ msg: err });
-            const msg = {
-                msg: "password successfully updated",
-                username: req.params.username,
-            };
-            return res.json(msg);
+router.patch("/update/:username", middleware.checkToken, async (req, res) => {
+    try {
+        const username = req.params.username;
+        const newPassword = req.body.password;
+
+        const user = await User.findOneAndUpdate(
+            { username: username },
+            { $set: { password: newPassword } }
+        );
+
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
         }
-    )
+
+        return res.json({
+            msg: "Password successfully updated",
+            username: username,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ msg: "Internal server error" });
+    }
 });
+
+router.delete("/delete/:username", middleware.checkToken, async (req, res) => {
+    try {
+        const username = req.params.username;
+
+        const user = await User.findOneAndDelete({ username: username });
+
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+
+        return res.json({
+            msg: "User deleted",
+            username: username,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ msg: "Internal server error" });
+    }
+});
+
 
 module.exports = router;
