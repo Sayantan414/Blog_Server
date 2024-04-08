201d68da39a153ab378b05d4eec8aaceba748732
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-04-08 22:10:27 +0530

    work

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const app = express();
 mongoose.connect("mongodb+srv://dipsadhu111:blogapp@blogapp.tosnqfm.mongodb.net/").then(() => console.log("connected to mongodb"));
 
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 const userRoute = require("./routes/user");
 app.use("/user", userRoute);
+const profileRoute = require("./routes/profile");
+app.use("/profile", profileRoute);
 
 
 app.route("/").get((req, res) => res.json("Your first rest api is here"));
